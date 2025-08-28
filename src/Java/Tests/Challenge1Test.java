8dc65bc589dfc4055a61954ca6595ee35fa8e4a1
Ian Hauser <ian.hauser@dev-pro.ca>
2025-08-28 15:36:15 -0600

    add score calculation scripts

---
+++ src/Java/Tests/Challenge1Test.java
@@ -0,0 +1,22 @@
+package Java.Tests;
+
+import Java.Challenges.Challenge1;
+
+public class ChallengeOneTest implements ChallengeTest{
+
+        @Override
+        public int runTests() {
+            Integer[] inputs = {
+                    8, 0, -1, 7, 32, -5, 6, 20, -12, -0
+            };
+            String[] expectedResults = {
+                    "12345678", "", "1", "1234567",
+                    "1234567891011121314151617181920212223242526272829303132",
+                    "12345", "123456", "1234567891011121314151617181920",
+                    "123456789101112", ""
+            };
+
+            return TestRunner.runTests(new Challenge1(), inputs, expectedResults);
+        }
+    }
+
---
+++ src/Java/Tests/ChallengeTest.java
@@ -0,0 +1,4 @@
+package Java.Tests;
+
+public interface ChallengeTest {
+}
